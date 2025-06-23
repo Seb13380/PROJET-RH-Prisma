63712690f60ae81882e6b6499bdced0dcb5be374
Seb13380 <sebcecg@gmail.com>
2025-06-23 07:10:20 +0200

    maj 23 juin sebbi

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -2,19 +2,22 @@ const { PrismaClient } = require("../../generated/prisma")
 const prisma = new PrismaClient({})
 
 exports.getHome = async (req, res) => {
-    try{
-        const user = await prisma.user.findUnique({
-            where:{
-                id:req.session.user.id
-            },
-            include:{
-                books:true
-            }
+    try {
+        const employes = await prisma.employe.findMany()
+        const ordinateurs = await prisma.ordinateurs.findMany({
+            include: { employe: true }
         })
-        res.render("pages/home.twig", {user: req.session.user, books:user.books})
-    }
-    catch(error){
-        res.render("pages/home.twig", {user: req.session.user})
+        res.render("pages/home.twig", {
+            user: req.session.user,
+            employes,
+            ordinateurs
+        })
+    } catch (error) {
+        res.render("pages/home.twig", {
+            user: req.session.user,
+            employes: [],
+            ordinateurs: []
+        })
     }
 }
 
